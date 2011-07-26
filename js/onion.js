537104fc79eec002b95e70d9d977a13bb1d717f3
craigdallimore <decoy9697@gmail.com>
2011-07-26 13:19:05 -0300

    Edited js/onion.js via GitHub

--- js/onion.js
+++ js/onion.js
@@ -5,19 +5,26 @@ TO GET THIS SCRIPT TO WORK, YOU NEED THE FOLLOWING INFORMATION:
 - THE HEIGHT OF YOUR COMP
 */
 $(document).ready(function() {
+
+	// SET YOUR INFORMATION HERE:
+	var config = {
+		path :	'',		// e.g. images/onion.jpg
+		width :	'',		// e.g. 960
+		height :''		// e.g. 1000
+	};
+
+
 	windowWidth = $(window).width();
 	windowHeight = $(window).height();
-	// CHANGE THIS TO THE CORRECT PATH AND WIDTH OF YOUR SITE COMP
-	$('body').prepend('<div id="comp"><img src="site_comps/comp.jpg" width="960" height="1000" /></div>');
+	$('body').prepend('<div id="comp"><img src="'+config.path+'" width="'+config.width+'" height="'+config.height+'" /></div>');
 	compWidth = $('#comp img').width();
 	compHeight = $('#comp img').height();
 	// POSITION THE COMP IN THE BACKGROUND
 	// CONSIDERATION NEEDS TO BE MADE FOR WHETHER THERE IS A SCROLL BAR OR NOT
 	if(windowHeight < compHeight){
 		$("#comp").css({
-			// ADJUST DIMENSIONS TO FIT YOUR SITE COMP
-			width: "960px",
-			height:"1000px",
+			width: config.width+"px",
+			height:config.height+"px",
 			position: "absolute",
 			top: "0",
 			// IF THERE IS A SCROLLBAR, WE NEED TO OFFSET THE LEFT POSITION OF THE COMP TO ACCOMMODATE IT (8PX HERE)
@@ -26,9 +33,8 @@ $(document).ready(function() {
 	}
 	else{
 		$("#comp").css({
-			// ADJUST DIMENSIONS TO FIT YOUR SITE COMP
-			width: "960px",
-			height:"1000px",
+			width: config.width+"px",
+			height:config.height+"px",
 			position: "absolute",
 			top: "0",
 			left: (((windowWidth - compWidth)/2))+"px"
@@ -41,7 +47,7 @@ $(document).ready(function() {
 		/* THIS MAY NEED TO BE ADJUSTED */
 		if(windowHeight < compHeight){
 			$("#comp").css({
-				left: (((windowWidth - compWidth)/2))+"px"
+				left: (((windowWidth - compWidth)/2)-8)+"px"
 			});
 		}
 		else{
@@ -50,4 +56,22 @@ $(document).ready(function() {
 			});
 		}
 	});
+	
+	
+	/* TOGGLE COMP VISIBILITY WITH TILDE KEY */
+	$(document).keyup(function(k){
+	
+		if(k.keyCode === 192 ){
+
+			if( $('#comp').is(':visible') ){
+				$('#comp').fadeOut();
+			} else {
+				$('#comp').fadeIn();
+			}	
+		}
+	
+	});
+	
+	
+	
 });
